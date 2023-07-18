71366b8aa157a2c26a0d227755b99022be6df1f1
LevelADude <jamalahmedgill363@gmail.com>
2023-07-18 19:59:30 +0200

    2.0

---
+++ Assets/Scripts/TeleportPair.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPair : MonoBehaviour
+{
+    public bool IsAvtive = true;
+    [SerializeField] private List<GameObject> tpPoints = new List<GameObject>();
+    [SerializeField] private Animator crossfadeAnimator;
+    [SerializeField] private float delay = 1f;
+
+    public void TeleportFrom(Transform transform)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (IsAvtive && player != null)
+        {
+            crossfadeAnimator.SetTrigger("Start");
+            StartCoroutine(TeleportTo(transform));
+        }
+    }
+
+    private IEnumerator TeleportTo(Transform transform)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        yield return new WaitForSeconds(delay);
+        if (transform == tpPoints[0].transform)
+        {
+            tpPoints[1].GetComponent<TeleportPoint>().IsAvtive = false;
+            player.transform.position = tpPoints[1].transform.position;
+        }
+        else
+        {
+            tpPoints[0].GetComponent<TeleportPoint>().IsAvtive = false;
+            player.transform.position = tpPoints[0].transform.position;
+        }
+        crossfadeAnimator.SetTrigger("End");
+    }
+}
---
+++ Assets/Scripts/TeleportPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPoint : MonoBehaviour
+{
+    public bool IsAvtive = true;
+    [SerializeField] private GameObject teleportPairManager;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && IsAvtive) {
+            teleportPairManager.GetComponent<TeleportPair>().TeleportFrom(transform);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            IsAvtive = true;
+        }
+    }
+}
