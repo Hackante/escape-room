3d77204def1a31c502a0b2aea377b318d8baa33d
Hackante <kante.hagen@gmail.com>
2023-07-26 21:12:40 +0200

    Added Modals

---
+++ Assets/Scripts/ModalTrigger.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ModalTrigger : MonoBehaviour
+{
+    public string title;
+    public string description;
+    public Sprite banner;
+    public bool triggerOnEnable;
+    public UnityEvent onContinueEvent;
+    public UnityEvent onCancelEvent;
+    public UnityEvent onAlternateEvent;
+
+    private void OnEnable()
+    {
+        if (!triggerOnEnable) return;
+
+        ShowModal();
+
+        Action continueCallback = null;
+        Action cancelCallback = null;
+        Action alternateCallback = null;
+
+        if (onContinueEvent.GetPersistentEventCount() > 0)
+        {
+            continueCallback = onContinueEvent.Invoke;
+        }
+        if (onCancelEvent.GetPersistentEventCount() > 0)
+        {
+            cancelCallback = onCancelEvent.Invoke;
+        }
+        if (onAlternateEvent.GetPersistentEventCount() > 0)
+        {
+            alternateCallback = onAlternateEvent.Invoke;
+        }
+
+        ModalWindowPanel.Instance().ShowAsVertical(title, description, banner, continueCallback, cancelCallback, alternateCallback);
+    }
+
+    public void ShowModal()
+    {
+        ModalWindowPanel.Instance().gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/ModalWindowPanel.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ModalWindowPanel : MonoBehaviour
+{
+    [Header("Header")]
+    [SerializeField] private Transform _headerArea;
+    [SerializeField] private TextMeshProUGUI _titleField;
+
+    [Header("Body")]
+    [SerializeField] private Transform _bodyArea;
+    [SerializeField] private Transform _verticalLayoutArea;
+    [SerializeField] private Image _bannerImage;
+    [SerializeField] private TextMeshProUGUI _descriptionText;
+    [Space()]
+    [SerializeField] private Transform _horizontalLayoutArea;
+    [SerializeField] private Transform _iconArea;
+    [SerializeField] private Image _iconImage;
+    [SerializeField] private TextMeshProUGUI _iconText;
+
+    [Header("Footer")]
+    [SerializeField] private Transform _footerArea;
+    [SerializeField] private Button _confirmButton;
+    [SerializeField] private Button _cancelButton;
+    [SerializeField] private Button _alternateButton;
+
+    private Action onConfirmAction;
+    private Action onDeclineAction;
+    private Action onAlternateAction;
+
+    public UnityEvent onContinueCallback;
+    public UnityEvent onCancelCallback;
+    public UnityEvent onAlternateCallback;
+
+    private static ModalWindowPanel _instance;
+
+    public static ModalWindowPanel Instance()
+    {
+        if (!_instance)
+        {
+            _instance = FindObjectOfType(typeof(ModalWindowPanel)) as ModalWindowPanel;
+            if (!_instance)
+                Debug.LogError("There needs to be one active ModalWindowPanel script on a GameObject in your scene.");
+        }
+
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+        Close();
+    }
+
+    public void Confirm()
+    {
+        Close();
+        onConfirmAction?.Invoke();
+    }
+
+    public void Decline()
+    {
+        Close();
+        onDeclineAction?.Invoke();
+    }
+
+    public void Alternate()
+    {
+        Close();
+        onAlternateAction?.Invoke();
+    }
+
+    public void Close()
+    {
+        this.gameObject.SetActive(false);
+
+    }
+
+    public void ShowAsVertical(string title, string description, Sprite banner, Action onConfirm, Action onDecline,
+        Action onAlternate = null, string confirmLabel = "Bestätigen", string declineLabel = "Abbrechen", string alternateLabel = null
+        )
+    {
+        _headerArea.gameObject.SetActive(string.IsNullOrEmpty(title) == false);
+        _titleField.text = title;
+
+        _descriptionText.text = description;
+        _bannerImage.sprite = banner;
+
+        _confirmButton.gameObject.SetActive(onConfirm != null);
+        _confirmButton.GetComponentInChildren<TextMeshProUGUI>().text = confirmLabel;
+        onConfirmAction = onConfirm;
+
+        _cancelButton.gameObject.SetActive(onDecline != null);
+        _cancelButton.GetComponentInChildren<TextMeshProUGUI>().text = declineLabel;
+        onDeclineAction = onDecline;
+
+        _alternateButton.gameObject.SetActive(onAlternate != null);
+        _alternateButton.GetComponentInChildren<TextMeshProUGUI>().text = alternateLabel;
+        onAlternateAction = onAlternate;
+
+        _horizontalLayoutArea.gameObject.SetActive(false);
+        _verticalLayoutArea.gameObject.SetActive(true);
+    }
+
+    public void ShowAsVertical(string title, string description, Sprite banner, Action onConfirm, string confirmLabel)
+    {
+        ShowAsVertical(title, description, banner, onConfirm, null, null, confirmLabel);
+    }
+
+    public void ShowAsVertical(string title, string description, Sprite banner, Action onConfirm)
+    {
+        ShowAsVertical(title, description, banner, onConfirm, null, null, "Weiter");
+    }
+
+    public void ShowAsVertical(string title, string description, Sprite banner, Action onConfirm, Action onDecline,
+        string confirmLabel, string declineLabel)
+    {
+        ShowAsVertical(title, description, banner, onConfirm, onDecline, null, confirmLabel, declineLabel);
+    }
+
+    public void ShowAsVertical(string title, string description, Sprite banner, Action onConfirm, Action onDecline)
+    {
+        ShowAsVertical(title, description, banner, onConfirm, onDecline, null, "Weiter", "Zurück");
+    }
+}
