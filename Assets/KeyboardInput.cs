b8bd760946c565c36cb266b8f9069f7ba830b8bc
LevelADude <jamalahmedgill363@gmail.com>
2023-12-26 21:23:11 +0100

    Touch Keyboard 01

--- Assets/KeyboardInput.cs
+++ Assets/KeyboardInput.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class KeyboardInput : MonoBehaviour
 {
+    private TMP_InputField inputField;
     public GameObject Object;
-    private InputField inputField;
+    
+
     // Start is called before the first frame update
     void Start()
     {   
-     inputField = Object.GetComponent<SetKeyboardInput>().GetInput();
+        inputField = Object.GetComponent<SetKeyboardInput>().GetInput();
+        inputField.text = "123";
     }
 
     // Update is called once per frame
@@ -19,13 +23,14 @@ public class KeyboardInput : MonoBehaviour
         
     }
     
-    public void Input(string input){
-        inputField.text += input;
+    public void Input(string input)
+    {
+        inputField.text += "input";
     }
 
     public void DeleteInput()
     {
-        if (inputField.text.Length > 0)
+        if (inputField != null && inputField.text.Length > 0)
         {
             inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
         }
--- Assets/SetKeyboardInput.cs
+++ Assets/SetKeyboardInput.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class SetKeyboardInput : MonoBehaviour
 {
-    public InputField inputField;
+    public TMP_InputField inputField;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public InputField GetInput()
+    public TMP_InputField GetInput()
     {
         return inputField;
     }
